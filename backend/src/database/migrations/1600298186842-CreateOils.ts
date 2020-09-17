db8701006fe3fb8569726c75a10f56372e105777
Your Name <you@example.com>
2020-09-16 21:07:06 -0300

    migrations

---
+++ backend/src/database/migrations/1600298186842-CreateOils.ts
@@ -0,0 +1,47 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateOils1600298186842 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'oils',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'uuid',
+                    isPrimary: false,
+                    generationStrategy: 'uuid',
+                    default: 'uuid_generate_v4()',
+                },
+                {
+                    name: 'name',
+                    type: 'varchar',
+                },
+                {
+                    name: 'expiration',
+                    type: 'timestamp with time zone',
+                },
+                {
+                    name: '',
+                    type: ''
+                },
+                {
+                    name: 'created_at',
+                    type: 'timestamp',
+                    default: 'now()',
+                },
+                {
+                    name: 'updated_at',
+                    type: 'timestamp',
+                    default: 'now()',
+                },
+
+            ]
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('oils');
+    }
+
+}
